c320d041173c245990e946b92bc918c8a7a23076
learning538 <118657148+learning538@users.noreply.github.com>
2024-02-27 15:26:54 +0530

    to use code
    please install nodemon, nodeEJS, and Package.json file on your Vs code

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const port =8080;
+const path =require("path");
+
+app.use(express.urlencoded({extended: true}));
+app.set ("view engine", "ejs");
+app.set("views",path.join(__dirname, "views"));
+app.use(express.static (path.join(__dirname, "public")));
+
+let posts =[
+    {
+        username: "Harshit dubey",
+        content: "I love coding",
+    },
+    {
+        username: "Raunak",
+        content: "I love Bolling",
+    },
+    {
+        username: "Adarsh",
+        content: "I love study",
+    },
+    {
+        username: "Harsh",
+        content: "I love my life",
+    },
+    {
+        username: "Varun",
+        content: "I am a star coder",
+    }
+    ,
+    {
+        username: "",
+        content: "",
+    }
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new", (req, res)=>{
+    res.render("form.ejs");
+});
+
+app.post("/posts", (req, res)=>{
+    let{username, content}=req.body;
+    posts.push({username, content});
+    let result =(req.body);
+    console.log(result);
+    res.redirect("/posts");
+});
+
+
+app.listen(port, () => {
+    console.log("listen to port : 8080");
+});
